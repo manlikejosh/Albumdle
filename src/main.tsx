7bfb2572625b311fcf72fea5ee89952a47afaa8a
Joshua Hodgson <114022342+manlikejosh@users.noreply.github.com>
2025-02-26 13:51:10 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
-
 import { BrowserRouter } from "react-router-dom";
-
+// - Return index.html for all routes
+// - This is automatic in Vite/CRA, but might need config in Nginx/Express
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
