c4e2f8fb84a8d0443c26c68f9fff2271ea17b861
Joshua Hodgson <114022342+manlikejosh@users.noreply.github.com>
2025-03-17 12:02:09 -0500

    Update apiHelper.ts

--- src/utilities/apiHelper.ts
+++ src/utilities/apiHelper.ts
@@ -1,24 +1,24 @@
 import { Album } from "../types/types";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export async function getDailyItem(): Promise<Album | null> {
   try {
-    const response = await fetch(
-      "https://albumdlebackend-production.up.railway.app/daily-item"
-    );
+    const response = await fetch(`${apiUrl}/daily-item`);
 
-    console.log("Response Status:", response.status);
-    console.log("Response URL:", response.url);
+    // console.log("Response Status:", response.status);
+    // console.log("Response URL:", response.url);
 
     // Check the content type and log the raw response body
     const rawResponse = await response.text(); // get raw response as text
-    console.log("Raw Response Body:", rawResponse);
+    // console.log("Raw Response Body:", rawResponse);
 
     if (!response.ok) {
       throw new Error(`Failed to fetch daily item: ${response.statusText}`);
     }
 
     const data = JSON.parse(rawResponse); // manually parse response
-    console.log("Parsed Data:", data);
+    // console.log("Parsed Data:", data);
     return data.item;
   } catch (error) {
     console.error("Error fetching daily item:", error);
@@ -28,17 +28,15 @@ export async function getDailyItem(): Promise<Album | null> {
 
 export async function getAllItems(): Promise<Album[]> {
   try {
-    const response = await fetch(
-      "https://albumdlebackend-production.up.railway.app/all-items"
-    );
+    const response = await fetch(`${apiUrl}/all-items`);
 
-    const text = await response.text(); // Get raw response
-    console.log("Raw Response:", text); // Log raw response for debugging
+    const text = await response.text();
+    // console.log("Raw Response:", text);
 
     if (!response.ok)
       throw new Error(`Failed to fetch items: ${response.statusText}`);
 
-    const data = JSON.parse(text); // Parse JSON manually
+    const data = JSON.parse(text);
     return data;
   } catch (error) {
     console.error("Error fetching items:", error);
